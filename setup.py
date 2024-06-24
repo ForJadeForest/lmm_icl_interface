fef1f7e89d2c874286ef12c0ba749c6c6fa45a3b
YingzhePeng <yingzhepeng@foxmail.com>
2024-06-24 10:16:19 +0800

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="lmm_icl_interface",
+    version="0.1",
+    author="YingzhePeng",
+    author_email="yingzhe.peng@foxmail.com",
+    description="",
+    # 你要安装的包，通过 setuptools.find_packages 找到当前目录下有哪些包
+    packages=find_packages(),
+)
